f1aa26f40c2e74b1e5a4bef040ab3c500711ef87
Josh Arcaya <2016094279@ust-ics.mygbiz.com>
2019-11-30 12:06:41 +0800

    Initial Commit

---
+++ TestAppForTJ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestAppForTJ
+//
+//  Created by Josh De Castro on 11/30/19.
+//  Copyright © 2019 Josh De Castro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
